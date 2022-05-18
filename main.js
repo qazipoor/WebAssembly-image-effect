f233e79aa695dd1faa1bcc0ba9c1f3c24919acff
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-18 20:38:54 +0300

    Encoding an image

--- main.js
+++ main.js
@@ -18,7 +18,10 @@ async function init() {
         let base64 = fileReader.result.replace(
             /^data:image\/(png|jpeg|jpg);base64,/, ''
         )
-        rustApp.grayscale(base64);
+        let img_data_url = rustApp.grayscale(base64);
+        document.getElementById('new-img').setAttribute(
+            'src', img_data_url
+        )
     }
 
     input.addEventListener('change', () => {
